038f58273ea058dedcf6291ec436a2f68f92a33c
AkhminDenis <denis-akhmin@yandex.ru>
2026-03-30 19:43:17 +0300

    добавление тестов для сервиса UserDetailsServiceImpl

---
+++ src/test/java/autoservice/model/service/UserDetailsServiceImplTest.java
@@ -0,0 +1,48 @@
+package autoservice.model.service;
+
+import autoservice.model.entities.User;
+import autoservice.model.enums.Role;
+import autoservice.model.exceptions.NotFoundException;
+import autoservice.model.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class UserDetailsServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Test
+    void loadUserByUsername_ShouldReturnUserDetails_WhenUserExists() {
+        User user = new User();
+        user.setUsername("username");
+        given(userRepository.findByUsername("username")).willReturn(Optional.of(user));
+
+        UserDetails result = userDetailsService.loadUserByUsername("username");
+
+        assertEquals(user, result);
+    }
+
+    @Test
+    void loadUserByUsername_ShouldThrowUsernameNotFoundException_WhenUserDoesNotExist () {
+        given(userRepository.findByUsername("username")).willReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class,
+                () -> userDetailsService.loadUserByUsername("username"));
+    }
+}
